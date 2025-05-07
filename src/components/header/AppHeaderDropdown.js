393e01186209cabdd4b4a7041e766578def51d66
Yesika Roa <yroa1402@gmail.com>
2025-05-07 18:16:07 -0400

    feat: Update user data handling, improve avatar upload functionality, and fix minor UI issues

--- src/components/header/AppHeaderDropdown.js
+++ src/components/header/AppHeaderDropdown.js
@@ -1,51 +1,51 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import {
   CAvatar,
-  CBadge,
   CDropdown,
   CDropdownDivider,
-  CDropdownHeader,
   CDropdownItem,
   CDropdownMenu,
   CDropdownToggle,
 } from '@coreui/react'
-import {
-  cilBell,
-  cilCreditCard,
-  cilCommentSquare,
-  cilEnvelopeOpen,
-  cilFile,
-  cilLockLocked,
-  cilSettings,
-  cilTask,
-  cilUser,
-  cilExitToApp,
-  cilPencil,
-} from '@coreui/icons'
+import { cilUser, cilExitToApp } from '@coreui/icons'
 import CIcon from '@coreui/icons-react'
-import { useNavigate } from 'react-router-dom' // Importa useNavigate
-
-import avatar8 from './../../assets/images/avatars/8.jpg'
+import { useNavigate } from 'react-router-dom'
 
 const AppHeaderDropdown = () => {
   const navigate = useNavigate() // Inicializa useNavigate
+  const [avatar, setAvatar] = useState(null) // Estado para almacenar el avatar del usuario
+
+  useEffect(() => {
+    const userId = localStorage.getItem('userId') // Obtén el ID del usuario desde localStorage
+    if (userId) {
+      // Realiza una solicitud para obtener los datos del usuario
+      fetch(`http://localhost:8000/users/${userId}`)
+        .then((response) => response.json())
+        .then((data) => {
+          setAvatar(data.avatar) // Actualiza el estado con la URL del avatar
+        })
+        .catch((error) => console.error('Error al obtener los datos del usuario:', error))
+    }
+  }, [])
 
   const handleLogout = () => {
     localStorage.removeItem('authToken') // Elimina el token de autenticación
+    localStorage.removeItem('userId') // Elimina el ID del usuario
     navigate('/login') // Redirige al login
   }
+
   const handleProfileClick = () => {
     navigate('/profile') // Redirige a la ruta del perfil
   }
+
   return (
     <CDropdown variant="nav-item">
       <CDropdownToggle placement="bottom-end" className="py-0 pe-0" caret={false}>
-        <CAvatar src={avatar8} size="md" />
+        <CAvatar src={avatar || 'default-avatar.jpg'} size="md" />{' '}
+        {/* Muestra un avatar por defecto si no hay avatar */}
       </CDropdownToggle>
       <CDropdownMenu className="pt-0" placement="bottom-end">
         <CDropdownItem onClick={handleProfileClick}>
-          {' '}
-          {/* Cambia href por onClick */}
           <CIcon icon={cilUser} className="me-2" />
           Profile
         </CDropdownItem>
